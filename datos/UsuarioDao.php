d6069fc819f781a20dbf8a1a4ed387f2c065dcca
xaulosky <pascifuentes@gmail.com>
2019-10-11 01:09:17 -0300

    Solucion error de conexion

--- datos/UsuarioDao.php
+++ datos/UsuarioDao.php
@@ -4,6 +4,7 @@ include 'Conexion.php';
 include '../entidades/Usuario.php';
 class UsuarioDao extends Conexion{
     protected static $cnx;
+
     private static function getConexion(){
         self::$cnx = Conexion::conectar();
     }
@@ -13,20 +14,20 @@ class UsuarioDao extends Conexion{
     }
 
     /**
-     * Metodo que sirve para validad el login
-     * @param objet     $usuario
-     * @return boolean
+     * Metodo que sirve para validar el login
+     * 
+     * @param   object     $usuario
+     * @return  boolean
      */
     public static function login($usuario){
         $query = "SELECT id,nombre,usuario,email,privilegio,fecha_registro FROM  usuarios WHERE usuario = :usuario AND password = :password";
         self::getConexion();
 
         $resultado = self::$cnx->prepare($query);
-        $resultado->bindParam(":usuario",$usuario->getUsuario());
-        $resultado->bindParam(":password",$password->getPassword());
+        $resultado->bindParam(":usuario", $usuario->getUsuario());
+        $resultado->bindParam(":password", $usuario->getPassword());
 
         $resultado->execute();
-
         if(count($resultado)){
             return true;
         }
